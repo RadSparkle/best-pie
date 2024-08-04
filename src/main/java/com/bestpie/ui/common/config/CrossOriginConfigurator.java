951af83a897b7c0a76f4809f6246f808b021c0d4
RadSparkle <wwwartg1@naver.com>
2024-08-04 16:43:01 +0900

    Feat : CrossOrigin 관련 설정클래스 추가

---
+++ src/main/java/com/bestpie/ui/common/config/CrossOriginConfigurator.java
@@ -0,0 +1,17 @@
+package com.bestpie.ui.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CrossOriginConfigurator implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .maxAge(3600);
+    }
+}
